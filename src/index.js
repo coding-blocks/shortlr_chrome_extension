fe58f38f8d85188a3f640bb8410209948a873bc6
Piyush <piyushnarang@dtu.ac.in>
2017-05-15 00:04:48 +0530

    enable one-clik url shortening

--- src/index.js
+++ src/index.js
@@ -1,52 +1,29 @@
 $(function () {
 
-    //booleans to control clicks
-    var b=false,a = true;
-    
     document.getElementById('open').style.cursor = "pointer";
+    console.log("test");
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
 
-    $('#box').click(function(){
-        if(!b){
-
-        chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-        var url = tabs[0].url;
-        console.log(url);
-
-        $.post("http://cb.lk/api/v1/shorten", {
+    $.post("http://cb.lk/api/v1/shorten", {
             url: url,
             code: "",
             secret: ""
 
         }, function (data) {
             console.log(data);
-
             $('#box').html("http://cb.lk/" + data.shortcode);
-            b = true;
+
+            var temp = $('<input>');
+            temp.val($('#box').text());
+            console.log(temp.val());
+            $('body').append(temp);
+            temp.select();
+            console.log(document.execCommand('copy'));
+            temp.remove();
+            $('#box').html("The Shortened Link has been copied to clipboard");            
+            
         });
-
-
-    });
-    }
-
-
-
-    })
-
+     });
     
-
-    $('#open').click(function () {
-        if(b&&a){var temp = $('<input>');
-        temp.val($('#box').text());
-        console.log(temp.val());
-        $('body').append(temp);
-        temp.select();
-        console.log(document.execCommand('copy'));
-        temp.remove();
-        $('#box').html("The Link has been copied to clipboard");
-    a = false;
-    }
     });
-
-
-});
-
