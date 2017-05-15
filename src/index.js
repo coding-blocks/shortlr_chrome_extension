48bdc8f789161de66a8f8c0a00411310193ab01e
Pawan Bansal <bansal.pawan96@gmail.com>
2017-05-15 15:50:19 +0530

    one click shortening with copy button

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 $(function () {
 
-    //booleans to control clicks
-    var b=false,a = true;
-    
-    document.getElementById('open').style.cursor = "pointer";
+    //variable holding the setTimeout
+    var timer;
 
-    $('#box').click(function(){
-        if(!b){
+    document.getElementById('copyBtn').style.cursor = "pointer";
 
-        chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    //Generation of short url and displaying it
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         var url = tabs[0].url;
         console.log(url);
 
@@ -16,37 +14,44 @@ $(function () {
             url: url,
             code: "",
             secret: ""
-
         }, function (data) {
             console.log(data);
 
             $('#box').html("http://cb.lk/" + data.shortcode);
             b = true;
         });
-
-
     });
-    }
 
-
-
-    })
-
-    
-
-    $('#open').click(function () {
-        if(b&&a){var temp = $('<input>');
+    //Defining and assigning click listener to copy button
+    $('#copyBtn').click(function () {
+        //Copying the url
+        var temp = $('<input>');
         temp.val($('#box').text());
         console.log(temp.val());
         $('body').append(temp);
         temp.select();
-        console.log(document.execCommand('copy'));
+        let copied = document.execCommand('copy');
+        console.log(copied);
         temp.remove();
-        $('#box').html("The Link has been copied to clipboard");
-    a = false;
-    }
-    });
 
+        //Displaying copy status to user
+        var msg = $('#msg');
+        if(copied){
+            msg.text('copied');
+            msg.css('color', 'green');
+        }
+        else{
+            msg.text('error');
+            msg.css('color', 'red');
+        }
+        msg.css('display', 'inline');
+        //clearing any previous timer
+        clearTimeout(timer);
+        //setting a timer to hide the 'copied' message after a second
+        timer = setTimeout(function () {
+            msg.css('display', 'none')
+        }, 1000);
+    });
 
 });
 
